b1b3d651a070787fb31a47f7b2daebca67896a21
Fatjon Rrapaj <38084457+FatjonRrapaj@users.noreply.github.com>
2022-11-09 09:15:56 +0100

    ongoing work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
+import HomePage from "./screens/Homepage";
+
 function App() {
-  return null;
+  //1. On render marr kategorite dhe produktet
+  //2. Kur klikon kategori merr produktet vetem per ate kategori
+  //3. Kur klikon produkt hap modalen dhe merr produktin me id
+  //4. kur i ben add to cart, e ruajme si produkt ne cart, dhe perditesohet ikona tek cart
+  //5. kur klikojme ikonen cart, shkojme ne screenin tjeter ku na afishohen te gjitha produktet dhe llogaritet totali
+  //6. ka vetem 2 screens dhe 1 modale. Mos beni cudira.
+
+  //dependencies to install:
+  //redux
+  //react-redux
+  //redux-thunk
+
+  return <HomePage />;
 }
 
 export default App;
